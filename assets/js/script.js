bdfec69e5e4fdbfdb86a5dd5b91235a68d712576
Wojciech Karakula <wojtekk304@gmail.com>
2023-10-01 22:53:44 +0000

    Add javascript code for easy quiz

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+let bleach_questions = [];
+const bleach_questions_easy = [
+    {
+        question: "Who was the first shinigami Ichigo met?",
+        answears: [
+            {text: "His Dad", correct: false},
+            {text: "Renji", correct: false},
+            {text: "Rukia", correct: true},
+            {text: "Yuzu", correct: false}
+        ]
+    },
+    {
+        question: "What is Ichigo's Bankai?",
+        answears: [
+            {text: "Tensa Zangetsu", correct: true},
+            {text: "Tenshi Zangetsu", correct: false},
+            {text: "It was never said", correct: false},
+            {text: "Getsuga Tensho", correct: false}
+        ]
+    },
+    {
+        question: "Who is the strongest espada in terms of their number?",
+        answears: [
+            {text: "Ulquiorra", correct: false},
+            {text: "Yammy", correct: true},
+            {text: "Stark", correct: false},
+            {text: "Halibel", correct: false}
+        ]
+    },
+    {
+        question: "What is Aizen's shikai?",
+        answears: [
+            {text: "Kyoukosuigetsu", correct: false},
+            {text: "Kyoukasuigetsu", correct: true},
+            {text: "Kyoukasuigitsu", correct: false},
+            {text: "Kyoukasoigetso", correct: false}
+        ]
+    },
+    {
+        question: "What purpose did Aizen give Wonderwise?",
+        answears: [
+            {text: "Kill Yamamoto", correct: false},
+            {text: "To protect him", correct: false},
+            {text: "Just like any other arrancar", correct: false},
+            {text: "Seal Ryujin Jakka", correct: true}
+        ]
+    },
+    {
+        question: "What did Tosen replace his Bankai with?",
+        answears: [
+            {text: "Not replaced", correct: false},
+            {text: "Hollowification", correct: true},
+            {text: "An ultimate attack", correct: false},
+            {text: "Different bankai", correct: false}
+        ]
+    },
+    {
+        question: "What is Komamura's bankai ability?",
+        answears: [
+            {text: "Summons a giant head with lazer eyes", correct: false},
+            {text: "Summons a giant arm holding a sword", correct: false},
+            {text: "Summon a giant Swordsman", correct: true},
+            {text: "Summons a giant fist with knuckles", correct: false}
+        ]
+    },
+    {
+        question: "What is the name of Ichigo's dad?",
+        answears: [
+            {text: "Ishin", correct: false},
+            {text: "Hishin", correct: false},
+            {text: "Ishon", correct: false},
+            {text: "Isshin", correct: true}
+        ]
+    },
+    {
+        question: "After Ichigo saved Rukia, what was given to him by the thirteen protection squads?",
+        answears: [
+            {text: "Nothing", correct: false},
+            {text: "Captain's jacket", correct: false},
+            {text: "Party", correct: false},
+            {text: "Substitute shinigami badge", correct: true}
+        ]
+    },
+    {
+        question: "Who injured Hinamori when everyone was fighting Aizen?",
+        answears: [
+            {text: "Aizen", correct: false},
+            {text: "Matsumoto", correct: false},
+            {text: "Halibel's fracion", correct: false},
+            {text: "Toshiro", correct: true}
+        ]
+    }
+];
